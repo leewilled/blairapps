4f9a69707c72336ec2a2eb2a25a1d95849ff6300
emilyliublair <67622097+emilyliublair@users.noreply.github.com>
2021-09-11 18:16:10 -0400

    down.sql

--- cms/migrations/setup/down.sql
+++ cms/migrations/setup/down.sql
@@ -1,3 +1,11 @@
 DROP TABLE auth_val;
 
 DROP TABLE events;
+DROP TABLE teachers;
+DROP TABLE announcements;
+DROP TABLE clubs;
+DROP TABLE lunch_events;
+DROP TABLE ssl_opps;
+DROP TABLE polls;
+DROP TABLE new;
+DROP TABLE important;
